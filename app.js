659b4c8b4c16bfc24de2c16c7d7d24708ad4a6aa
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-09 18:28:23 +0100

    dadi

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- let email = prompt ('Ciao ,dimmi la tua email per verificare che sei registrato!');
+/*  let email = prompt ('Ciao ,dimmi la tua email per verificare che sei registrato!');
 
 const emailRegistrate = [
     'ciccio@gmail.com',
@@ -20,6 +20,26 @@ for ( i = 0 ; i < emailRegistrate.length ; i++){
     }
     
 } 
+ */
+
+
+let mioNumero = prompt('scegli un numero da 1 a 6!')
+if (mioNumero > 6){
+    alert('ho detto da 1 a 6...')
+}
+
+
+ let computerNumero = Math.floor(Math.random() * 6) + 1
+if ( mioNumero > computerNumero){
+    alert('Daje, hai vinto!!')
+}
+else if (mioNumero < computerNumero ){
+    alert('Sei sfortunato ,riprova!')
+}
+else if (mioNumero == computerNumero){
+    alert('uhhh , sono uguali. Quindi non hai vinto !!')
+}
+
 
     
 
